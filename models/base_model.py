df07e358bfd34b5b6e28084527cd616e4780a2b0
ahmed711 <ahmed_tayseer12@yahoo.com>
2024-01-14 14:14:45 -0800

    Modify Ex3

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,33 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            if '__class__' in kwargs:
+                del kwargs['__class__']
+            self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        id = self.id
+        dict = self.__dict__
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, id, dict)
 
     def to_dict(self):
         """Returns a dictionary representation of the object"""
         obj_dict = self.__dict__
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                obj_dict[key] = value.isoformat()
+            else:
+                obj_dict[key] = value
 
         return obj_dict
